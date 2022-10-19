a139e1cf85ea724d3d693403cf41bfaaf0f585ee
Gustavo H. Fazam <gufazam10@gmail.com>
2022-10-18 22:28:15 -0300

    Programa para converter Dólar em Real

---
+++ app.js
@@ -0,0 +1,5 @@
+function ConverterDeDolarParaReal(){
+    let valorEmDolar = document.getElementById("valor").value
+    let valorConvertido = valorEmDolar * 5.3
+    document.getElementById("valorConvertido").innerText = valorConvertido.toFixed(2)
+}
